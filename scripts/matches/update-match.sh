851e3b23e24cd6ab690852a51453d4c6135dca17
Ash Trull <ashtrull@gmail.com>
2017-10-03 15:53:58 -0400

    All curl scripts tested on local and heroku
    Add curl scripts for all CRUD functions on all resources
    All functional on local server and heroku
    Database is set up and ready to be connected to front end

--- scripts/matches/update-match.sh
+++ scripts/matches/update-match.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-API="${API_ORIGIN:-http://localhost:4741}"
-# API="${API_ORIGIN:-https://seedsower.herokuapp.com}"
+# API="${API_ORIGIN:-http://localhost:4741}"
+API="${API_ORIGIN:-https://amoridarity.herokuapp.com/}"
 URL_PATH="/matches/${ID}"
 curl "${API}${URL_PATH}" \
   --include \
--- scripts/offers/create-offer.sh
+++ scripts/offers/create-offer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-API="${API_ORIGIN:-http://localhost:4741}"
-# API="${API_ORIGIN:-https://seedsower.herokuapp.com}"
+# API="${API_ORIGIN:-http://localhost:4741}"
+API="${API_ORIGIN:-https://amoridarity.herokuapp.com/}"
 URL_PATH="/offers"
 curl "${API}${URL_PATH}" \
   --include \
